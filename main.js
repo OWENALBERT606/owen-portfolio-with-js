fbbadbac88eb5e79cf864bb31de31f6dffee77df
OWENALBERT606 <owenalbert606@gmail.com>
2023-02-19 14:23:56 -0800

    eddd

---
+++ main.js
@@ -0,0 +1,6 @@
+// console.log("its working");
+const menuBtn = document.querySelector("#menu");
+const nav = document.querySelector("navigation");
+menuBtn.addEventListener("click",function(){
+    nav.style.display ="flex"
+})
