56c61d41eb3fde9a1240267c9754651d46140ef9
OWENALBERT606 <owenalbert606@gmail.com>
2023-06-06 20:31:29 +0300

    new commit

--- main.js
+++ main.js
@@ -4,3 +4,29 @@ const nav = document.querySelector(".navigation");
 menuBtn.addEventListener("click",function(){
     console.log(nav);
     nav.classList.toggle("active");})
+
+
+    const animation_elements =document.querySelectorAll('.animate-on-scroll,.animate-top-to-btm');
+// console.log(animation_elements);
+
+
+
+
+function callback(entries){
+    entries.forEach((entry) =>{
+       if(entry.IsIntersecting){
+        entry.target.classList.add("animate");
+       } else{
+        entry.target.classList.remove("animate");
+       };
+    });
+};
+const options= {
+    threshold:0.5,
+};
+
+const observer = new IntersectionObserver(callback,options);
+
+animation_elements.forEach((element)=>{
+    observer.observe(element);
+});
